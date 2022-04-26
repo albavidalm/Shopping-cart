594108d70cc1f876082bbb76ffec2982ef18f42d
albavidalm <albavidalm@gmail.com>
2022-04-26 18:04:00 +0200

    Create selected course Object

--- js/app.js
+++ js/app.js
@@ -3,14 +3,30 @@ const cartContainer = document.querySelector("#lista-carrito tbody");
 const emptyCartBtn = document.querySelector("#vaciar-carrito");
 const coursesList = document.querySelector("#lista-cursos");
 
-const uploadListeners = () => {
-  coursesList.addEventListener("click", addCourse);
-};
+/*const uploadListeners = () => {
+coursesList.addEventListener("click", addCourse);
+};*/
 
 const addCourse = (ev) => {
   ev.preventDefault();
   if (ev.target.classList.contains("agregar-carrito")) {
-    console.log(ev.target);
+    const selectedCourse = ev.target.parentElement.parentElement;
+    getCourseData(selectedCourse);
   }
 };
-uploadListeners();
+
+const getCourseData = (course) => {
+  const infoCourse = {
+    image: course.querySelector("img").src,
+    title: course.querySelector("h4").textContent,
+    price: course.querySelector(".precio span").textContent,
+    id: course.querySelector("a").getAttribute("data-id"),
+    quantity: 1,
+  };
+  console.log(infoCourse);
+};
+
+//uploadListeners();
+
+// Events ----------
+coursesList.addEventListener("click", addCourse);
